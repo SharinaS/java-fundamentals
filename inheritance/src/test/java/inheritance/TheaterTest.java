b18d3b713b114b3364d765b8aecb459d6d918482
Sharina Stubbs <sharinastubbs@gmail.com>
2019-09-28 13:53:39 -0700

    finished theater class, added tests and updated readme

--- inheritance/src/test/java/inheritance/TheaterTest.java
+++ inheritance/src/test/java/inheritance/TheaterTest.java
@@ -29,7 +29,50 @@ public class TheaterTest {
 
     // add a movie to empty list
     @Test
-    public void testAddMovie() {
-
+    public void testAddMovie1() {
+        happyNightsTheater.addMovie("Cloudy Days");
+        assertEquals(
+                "movie list should have the movie, Cloudy Days, in it",
+                "The theater Happy Nights Theater is showing these movies: [Cloudy Days].",
+                happyNightsTheater.toString());
     }
+
+    // add two movies to list
+    @Test
+    public void testAddMovie2() {
+        happyNightsTheater.addMovie("Cloudy Days");
+        happyNightsTheater.addMovie("Abracadabra");
+        assertEquals(
+                "movie list should have the movies, Cloudy Days and Abracadabra, in it",
+                "The theater Happy Nights Theater is showing these movies: [Cloudy Days, Abracadabra].",
+                happyNightsTheater.toString());
+    }
+
+    // remove last movie from the list
+    @Test
+    public void testRemoveMovie1() {
+        happyNightsTheater.addMovie("Cloudy Days");
+        happyNightsTheater.addMovie("Abracadabra");
+        happyNightsTheater.removeMovie("Abracadabra");
+        assertEquals(
+                "only one movie should be left in the list - Cloudy Days",
+                "The theater Happy Nights Theater is showing these movies: [Cloudy Days].",
+                happyNightsTheater.toString()
+        );
+    }
+
+    // remove last movie from the list
+    @Test
+    public void testRemoveMovie2() {
+        happyNightsTheater.addMovie("Cloudy Days");
+        happyNightsTheater.addMovie("Abracadabra");
+        happyNightsTheater.addMovie("Happy Leaves");
+        happyNightsTheater.removeMovie("Abracadabra");
+        assertEquals(
+                "Two movies should be left in the list",
+                "The theater Happy Nights Theater is showing these movies: [Cloudy Days, Happy Leaves].",
+                happyNightsTheater.toString()
+        );
+    }
+
 }
